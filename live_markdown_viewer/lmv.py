6ae3839b31ae733163f1009e49cffb5bafc4d521
Hugo Klepsch <hugo.klepsch@gmail.com>
2019-09-08 13:11:33 -0400

    Basic file watching implemented, MVP done
    * Update Pylint to ignore PyQt5. Pylint doesn't seem to work with the
        package.

--- live_markdown_viewer/lmv.py
+++ live_markdown_viewer/lmv.py
@@ -3,9 +3,11 @@ Live markdown viewer
 """
 from argparse import ArgumentParser
 from io import BytesIO
+from time import sleep
 
 import markdown
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTextBrowser
+from PyQt5.QtCore import QFileSystemWatcher, QFileInfo
 
 
 class LiveMarkdownViewer(QApplication):
@@ -25,8 +27,37 @@ class LiveMarkdownViewer(QApplication):
         self.viewer = QTextBrowser()
         self.update_viewer()
 
+        self.watcher = QFileSystemWatcher()
+        self.watcher.addPath(self.filename)
+        self.watcher.fileChanged.connect(self._file_change_handler)
+
         self.window.setCentralWidget(self.viewer)
         self.window.show()
+
+    def _file_change_handler(self, path):
+        """
+        https://stackoverflow.com/a/30076119
+        Some text editors delete and re-write the file when they save. When the file is deleted, Qt will stop
+        watching it. To get around this, we can wait a short time to see if the file returns.
+        :param str path: The path of the changed file.
+        """
+        file_info = QFileInfo(path)
+
+        time_slept = 0.0
+        sleep_for = 0.1
+
+        # Sleep up to 1 second while waiting for the file to be re-created
+        while not file_info.exists() and time_slept < 1.0:
+            time_slept += sleep_for
+            sleep(sleep_for)
+
+        if file_info.exists():
+            self.watcher.addPath(path)
+
+            self.update_viewer()
+        else:
+            # File deleted ?
+            pass
 
     def render_to_html_string(self):
         """
@@ -39,6 +70,7 @@ class LiveMarkdownViewer(QApplication):
         return str(fake_file.read(), 'utf-8')
 
     def update_viewer(self):
+        """Update the viewer"""
         html = self.render_to_html_string()
         self.viewer.setHtml(html)
 
--- test_files/simple.md
+++ test_files/simple.md
@@ -5,8 +5,5 @@ Here is some text
 ## This will have a four space indented list
 - Item one
     - Item indented 4
----
-## This will have a two space indented list
-- Item one
-    - Item indented 2
+    - Another item
 ---
